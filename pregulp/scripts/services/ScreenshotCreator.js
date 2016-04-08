21846d15a954a78d34142bd0b194c0c5b1d029ad
IanMcLeodTombola <ian.mcleod@tombola.com>
2016-04-08 16:38:30 +0100

    Removed console logs, added logo

--- pregulp/scripts/services/ScreenshotCreator.js
+++ pregulp/scripts/services/ScreenshotCreator.js
@@ -6,18 +6,14 @@
             var me = this;
 
             me.downloadPromt = function(image){
-                console.log('attempting to download');
-                var link = document.createElement('a');
+                var link = document.createElement('a'); //hacky lol
                 link.download = 'testname.png';
                 link.href = image;
                 link.click();
             };
 
             me.createImage = function(){
-                console.log('attempting to create image');
                 me.image = globalCanvas.getCanvas().toDataURL("image/png").replace("image/png", "image/octet-stream");
-                console.log('imageCreated', typeof me.image, me.image);
-
                 me.downloadPromt(me.image);
             };
         }])
